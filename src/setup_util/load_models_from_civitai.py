305fd737ad09b7e7ae8a902f556c55e3fbb46d77
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-03-02 15:29:00 +0000

    Added progress bar

--- src/setup_util/load_models_from_civitai.py
+++ src/setup_util/load_models_from_civitai.py
@@ -24,10 +24,17 @@ def load_models_from_civitai(models:List[dict]):
 
     for model in models:
         r = requests.get(model['url'], stream=True)
+        # Get filename
         content_disposition = r.headers["content-disposition"]
         filename = content_disposition.split("filename=")[1].replace('"', '')
+        # Get size
+        total_size = int(r.headers.get("content-length", 0))
+        chunk_size = 10 * 1024
+
         filepath = os.path.join(model['local_dir'], filename)
-        
-        with open(filepath, mode="wb") as file:
-            for chunk in tqdm(r.iter_content(chunk_size=10 * 1024)):
-                file.write(chunk)
+
+        with tqdm(total=total_size, unit="B", unit_scale=True) as progress_bar:
+            with open(filepath, mode="wb") as file:
+                for chunk in r.iter_content(chunk_size):
+                    progress_bar.update(len(chunk))
+                    file.write(chunk)
