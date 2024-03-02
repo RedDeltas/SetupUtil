212c17d5b8550d1b65b541bea55a89429d15c9a3
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-03-02 11:39:23 +0000

    Added ability to download private repos

--- src/setup_util/load_models_from_huggingface.py
+++ src/setup_util/load_models_from_huggingface.py
@@ -2,10 +2,19 @@ import os
 import json
 from typing import List
 from huggingface_hub import hf_hub_download
+from huggingface_hub.utils._errors import RepositoryNotFoundError
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 
 def load_models_from_huggingface(models:List[dict]):
     for model in models:
-        hf_hub_download(**model)
+        try:
+            hf_hub_download(**model)
+        except RepositoryNotFoundError:
+            hf_token = input("Unauthorised, please enter token:")
+            os.environ["HF_TOKEN"] = hf_token
+            try:
+                hf_hub_download(**model)
+            except RepositoryNotFoundError:
+                raise Exception(f"Could not find HuggingFace repository: {model['repo_id']}")
