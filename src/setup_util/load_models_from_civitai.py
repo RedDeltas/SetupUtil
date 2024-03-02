1cbdc340b5b772b066793742013eb34ec7e2a847
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-03-02 15:41:54 +0000

    Removed unused handler

--- src/setup_util/load_models_from_civitai.py
+++ src/setup_util/load_models_from_civitai.py
@@ -4,22 +4,6 @@ import threading
 from tqdm import tqdm
 from typing import List
 
-def Handler(start, end, url, filename): 
-     
-    # specify the starting and ending of the file 
-    headers = {'Range': 'bytes=%d-%d' % (start, end)} 
-  
-    # request the specified part and get into variable     
-    r = requests.get(url, headers=headers, stream=True) 
-  
-    # open the file and write the content of the html page  
-    # into file. 
-    with open(filename, "r+b") as fp: 
-      
-        fp.seek(start) 
-        var = fp.tell() 
-        fp.write(r.content) 
-
 def load_models_from_civitai(models:List[dict]):
     print("Downloading models from CivitAI")
 
