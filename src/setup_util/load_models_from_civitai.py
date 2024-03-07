b7a0075a80f66f54936a3271d70f65079a56d547
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-03-07 18:55:26 +0000

    Added multithreading

--- src/setup_util/load_models_from_civitai.py
+++ src/setup_util/load_models_from_civitai.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import threading 
 from tqdm import tqdm
 from typing import List
 
@@ -10,8 +11,35 @@ def get_headers(token:str) -> dict:
     }
     return headers
 
+def download_model_part(start, end, url, filepath, filename, chunk_size):
+    if "CIVITAI_TOKEN" in os.environ:
+        civitai_token = os.getenv("CIVITAI_TOKEN")
+        headers = get_headers(civitai_token)
+    else:
+        headers = {}
+    
+    headers['Range'] = f'bytes={start}-{end}'
+
+    r = requests.get(url, headers=headers, stream=True)
+
+    # with open(filepath, "r+b") as file:
+    #     file.seek(start)
+    #     var = file.tell()
+    #     file.write(r.content)
+
+    with tqdm(total=end-start, unit="B", unit_scale=True) as progress_bar:
+            progress_bar.set_description(f'{filename}-{start}')
+            with open(filepath, mode="r+b") as file:
+                file.seek(start)
+                var = file.tell()
+                for chunk in r.iter_content(chunk_size):
+                    progress_bar.update(len(chunk))
+                    file.write(chunk)
+
 def load_models_from_civitai(models:List[dict]):
     print("Downloading models from CivitAI")
+
+    number_of_threads = 4
 
     for model in models:
         if "CIVITAI_TOKEN" in os.environ:
@@ -37,9 +65,32 @@ def load_models_from_civitai(models:List[dict]):
 
         filepath = os.path.join(model['local_dir'], filename)
 
-        with tqdm(total=total_size, unit="B", unit_scale=True) as progress_bar:
-            progress_bar.set_description(filename)
-            with open(filepath, mode="wb") as file:
-                for chunk in r.iter_content(chunk_size):
-                    progress_bar.update(len(chunk))
-                    file.write(chunk)
+        part = int(total_size / number_of_threads)
+
+        # Create file with size of content
+        with open(filepath, "wb") as file:
+            file.write(b'\0' * total_size)
+
+        for i in range(number_of_threads):
+            start = part * i
+            end = start + part
+
+            t = threading.Thread(
+                target=download_model_part,
+                kwargs={
+                    'start': start,
+                    'end': end,
+                    'url': model['url'],
+                    'filepath': filepath,
+                    'filename': filename,
+                    'chunk_size': chunk_size,
+                }
+            )
+            t.setDaemon(True)
+            t.start()
+        
+        main_thread = threading.current_thread()
+        for t in threading.enumerate():
+            if t is main_thread:
+                continue
+            t.join()
