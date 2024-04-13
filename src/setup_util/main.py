0eee3748953f06481f7250a41c9953318631aebe
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-04-13 10:19:36 +0000

    Added functionality to parse multiple config files

--- src/setup_util/main.py
+++ src/setup_util/main.py
@@ -6,18 +6,19 @@ from setup_util.load_models_from_civitai import load_models_from_civitai
 
 parser = ArgumentParser()
 
-parser.add_argument("config_file", help="Config file for setup (see example_configs in Github - https://github.com/RedDeltas/SetupUtil/tree/main/example_configs)")
+parser.add_argument("config_files", nargs="+", help="One or more config files for setup (see example_configs in Github - https://github.com/RedDeltas/SetupUtil/tree/main/example_configs)")
 
 args = parser.parse_args()
 
 def main():
     print(banner)
-    with open(args.config_file) as file:
-        config = json.load(file)
-    
-    if "hf_models" in config:
-        load_models_from_huggingface(config["hf_models"])
-    
-    if "civitai_models" in config:
-        load_models_from_civitai(config["civitai_models"])
+    for config_file in args.config_files:
+        with open(config_file) as file:
+            config = json.load(file)
+        
+        if "hf_models" in config:
+            load_models_from_huggingface(config["hf_models"])
+        
+        if "civitai_models" in config:
+            load_models_from_civitai(config["civitai_models"])
 
